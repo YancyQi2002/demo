38f25ba3484baa4b182022f0d7d809b978ca670d
YancyQi2002 <63138264+YancyQi2002@users.noreply.github.com>
2022-07-21 15:34:57 +0000

    first commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,13 +5,22 @@
     <div class="max-w-md w-full space-y-8">
       <div>
         <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 class="mt-6 text-3xl">
             Hello World!
         </h2>
+      </div>
+      <div>
+        <h4 class="flex m-6 justify-center text-2xl">
+            Vite 3 + Vue 3 TEMPLATE
+        </h4>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+h2,
+h4 {
+  @apply text-center font-extrabold text-gray-900 select-none;
+}
 </style>
