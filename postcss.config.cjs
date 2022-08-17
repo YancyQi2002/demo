0e733d41ad630d514fcb10c425bdd8d06c309bdb
YancyQi2002 <63138264+YancyQi2002@users.noreply.github.com>
2022-08-17 11:13:47 +0000

    twenty-third commit

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,16 @@
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {
       config: './tailwind.config.cjs'
     },
     autoprefixer: {},
-    'postcss-preset-env': {},
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': false
+      }
+    },
     'postcss-pxtorem': {},
   },
 }
